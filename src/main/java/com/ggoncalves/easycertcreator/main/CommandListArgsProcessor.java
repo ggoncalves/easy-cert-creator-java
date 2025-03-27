54bbdebd33b0ee4d1b5bd194e18bad0121528685
Gláucio Gonçalves <glaucio.c.goncalves@gmail.com>
2025-03-27 20:40:43 -0300

    Refactor argument validation and integrate ggutils.
    Replaced custom argument validation with ggutils library's `CommandProcessor` to simplify code and enhance maintainability. Removed redundant validation logic and associated tests, updated dependencies in `pom.xml`, and restructured `CommandListArgsProcessor` for streamlined initialization.

--- src/main/java/com/ggoncalves/easycertcreator/main/CommandListArgsProcessor.java
+++ src/main/java/com/ggoncalves/easycertcreator/main/CommandListArgsProcessor.java
@@ -1,95 +1,73 @@
 package com.ggoncalves.easycertcreator.main;
 
+import com.ggoncalves.ggutils.console.cli.CommandProcessor;
+import com.ggoncalves.ggutils.console.exception.FilePermissionException;
+import com.ggoncalves.ggutils.console.exception.InvalidFileException;
+import com.ggoncalves.ggutils.console.validation.FilePathValidator;
 import com.google.common.annotations.VisibleForTesting;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
-import java.io.File;
-
+@AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class CommandListArgsProcessor {
 
-  boolean process(String[] args) {
-    Options options = buildOptions();
-    CommandLineParser parser = new DefaultParser();
+  @Builder.Default
+  private CommandProcessor commandProcessor = new CommandProcessor(new FilePathValidator());
+
+  @Builder.Default
+  private String jasperFileOption = "c";
+
+  @Builder.Default
+  private String certInfoFileOption = "i";
+
+  @Builder.Default
+  private String outputOption = "o";
+
+  @Builder.Default
+  private String appName = "EasyCertCreator";
+
+  public void init() {
+    commandProcessor
+        .addRequiredOption(jasperFileOption, "jasperfile", true, "Compiled Jasper Template File")
+        .addRequiredOption(certInfoFileOption, "certinfofile", true, "Certification File Info (.txt file)")
+        .addRequiredOption(outputOption, "output", true, "Output directory to save .pdf files");
+  }
+
+
+  public void process(String[] args) {
     try {
-      CommandLine cmd = parser.parse(options, args);
+      if (!hasOptions()) {
+        init();
+      }
+
+      CommandLine cmd = commandProcessor.parseArgs(args);
       validateFiles(cmd);
-    } catch (ParseException e) {
-      System.err.println("EasyCertCreator error found: " + e.getMessage());
-      new HelpFormatter().printHelp("EasyCertCreator", options, true);
-      return false;
     }
-    return true;
-  }
-
-  private Options buildOptions() {
-    Options options = new Options();
-    options.addRequiredOption("c", "jasperfile", true, "Compiled Jasper Template File");
-    options.addRequiredOption("i", "certinfofile", true, "Certification File Info (.txt file)");
-    options.addRequiredOption("o", "output", true, "Output directory to save .pdf files");
-    return options;
-  }
-
-  private void validateFiles(CommandLine cmd) throws ParseException {
-    validateInputFile(cmd.getOptionValue("jasperfile"), "Jasper template");
-    validateInputFile(cmd.getOptionValue("certinfofile"), "Certificate info");
-    validateOutputDir(cmd.getOptionValue("output"));
-  }
-
-  private void validateInputFile(String path, String fileType) throws ParseException {
-    File file = getFileForPath(path);
-    assertFileExists(path, file);
-    assertIsFile(path, fileType, file);
-    assertCanRead(path, fileType, file);
-  }
-
-  private static void assertCanRead(String path, String fileType, File file) throws ParseException {
-    if (!file.canRead()) {
-      throw new ParseException(String.format("Cannot read the %s file (check permissions): %s", fileType, path));
+    catch (ParseException | InvalidFileException | FilePermissionException e) {
+      System.err.println(appName + " error found: " + e.getMessage());
+      commandProcessor.printHelp(appName);
     }
   }
 
-  private static void assertIsFile(String path, String fileType, File file) throws ParseException {
-    if (!file.isFile()) {
-      throw new ParseException(String.format("The %s path must be a file: %s", fileType, path));
-    }
-  }
-
-  private void validateOutputDir(String path) throws ParseException {
-    File dir = getFileForPath(path);
-    assertFileExists(path, dir);
-    assertIsDirectory(path, dir);
-    assertCanWriteToDirectory(path, dir);
-  }
-
-  private static void assertCanWriteToDirectory(String path, File dir) throws ParseException {
-    if (!dir.canWrite()) {
-      throw new ParseException(String.format("Cannot write to the output directory (check permissions): %s", path));
-    }
-  }
-
-  private static void assertIsDirectory(String path, File dir) throws ParseException {
-    if (!dir.isDirectory()) {
-      throw new ParseException(String.format("The output path must be a directory: %s", path));
-    }
-  }
-
-  private static void assertFileExists(String path, File file) throws ParseException {
-    if (!file.exists()) {
-      throw new ParseException(String.format("The output directory does not exist: %s", path));
-    }
+  protected void validateFiles(CommandLine cmd) {
+    commandProcessor.validateInputFile(cmd.getOptionValue(jasperFileOption), "Jasper template");
+    commandProcessor.validateInputFile(cmd.getOptionValue(certInfoFileOption), "Certificate info");
+    commandProcessor.validateOutputDir(cmd.getOptionValue(outputOption));
   }
 
   @VisibleForTesting
-  File getFileForPath(String path) {
-    return new File(path);
+  boolean hasOptions() {
+    try {
+      commandProcessor.parseArgs(new String[]{});
+      return true;
+    }
+    catch (ParseException e) {
+      return e.getMessage().contains("Missing required option");
+    }
   }
 }
